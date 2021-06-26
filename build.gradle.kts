12b3b189e5c369627add9bb3da370bfd196b442c
Sunghwan <nowy08@gmail.com>
2021-06-27 01:54:21 +0900

    dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         maven(url = "https://maven-central-asia.storage-download.googleapis.com/maven2/")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("com.android.tools.build:gradle:7.0.0-beta04")
         classpath(Dependencies.Kotlin.plugin)
     }
 }
