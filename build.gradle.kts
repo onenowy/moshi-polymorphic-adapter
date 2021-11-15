0e14e540a3ceb3a579a66df36800d6c516e6f1bd
onenowy <onenowy@gmail.com>
2021-11-15 19:14:17 +0900

    Release candidates 0.1.1 Fix nexus publish [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 nexusPublishing {
     repositories {
-        create("maven") {
+        create("sonatype") {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
             username.set(findProperty("ossrhUsername") as? String)
