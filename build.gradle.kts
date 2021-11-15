98e358cbd899d22bc2b2ea9672a32440196dc966
onenowy <onenowy@gmail.com>
2021-11-15 18:20:19 +0900

    Add nexus-publish plugin [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+}
+
+nexusPublishing {
+    repositories {
+        create("maven") {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            username.set(findProperty("ossrhUsername") as? String)
+            password.set(findProperty("ossrhPassword") as? String)
+        }
+    }
+}
+
 task("clean", Delete::class) {
     delete = setOf(rootProject.buildDir)
 }
